19650bbd8e0f7950dd6eab198b9e2abfaf539d1c
tostranger <11919psalms@gmail.com>
2021-11-13 09:14:52 +0900

    1113

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 $(function() {
-	$(document).ready(function() {});
+	$(document).ready(function() {
+		$(".credit").on("click", function() {
+			let title = $(this).data("title");
+			let account = $(this).data("account");
+			let bank = $(this).data("bank");
+			let name = $(this).data("name");
+			console.log(bank);
+			$(".modal-title").text(title);
+			$(".modal-bank").text(bank);
+			$(".modal-account").text(account);
+			$(".modal-accName").text(name);
+		});
+
+		var swiper = new Swiper(".mySwiper", {
+			loop: true,
+			spaceBetween: 10,
+			slidesPerView: 4,
+			freeMode: true,
+			watchSlidesProgress: true
+		});
+		var swiper2 = new Swiper(".mySwiper2", {
+			loop: true,
+			spaceBetween: 10,
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev"
+			},
+			thumbs: {
+				swiper: swiper
+			}
+		});
+	});
 });
