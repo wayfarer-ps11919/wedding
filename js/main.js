426a5c614b0c6ca19e91cc599fbe7a5028ae8937
tostranger <11919psalms@gmail.com>
2021-11-14 15:09:07 +0900

    111407

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
+function jsCopyLink(copyText) {
+	var tmpTextarea = document.createElement("textarea");
+	tmpTextarea.value = copyText;
+
+	document.body.appendChild(tmpTextarea);
+	tmpTextarea.select();
+	tmpTextarea.setSelectionRange(0, 9999); // 셀렉트 범위 설정
+
+	document.execCommand("copy");
+	document.body.removeChild(tmpTextarea);
+	alert("복사하였습니다.");
+}
 $(function() {
 	$(document).ready(function() {
 		// 모든 이미지 tag들을 list형태로 가져옴
 
 		$(".copy").on("click", function() {
 			let copymsg = $(".modal-account").text();
+			console.log(copymsg);
 			$(this).attr("data-clipboard-text", copymsg);
-
+			jsCopyLink(copymsg);
 			var clip = new Clipboard(".copy");
 			clip.on("success", function(e) {
 				alert("계좌번호를 복사하였습니다.");
 			});
+			navigator.clipboard
+				.writeText(`${copymsg}`)
+				.then(() => {
+					alert(`${copiedText}을 클립보드에 복사했습니다.`);
+				})
+				.catch(() => {
+					alert(`복사 실패!`);
+				});
 		});
 		$(".credit").on("click", function() {
 			let title = $(this).data("title");
