5a59855c212aaa2cd006fbb6c840fdb12e8bad57
tostranger <11919psalms@gmail.com>
2021-11-20 22:41:15 +0900

    112003

--- js/main.js
+++ js/main.js
@@ -28,14 +28,12 @@ $(function() {
 		});
 
 		var swiper = new Swiper(".mySwiper", {
-			loop: true,
 			spaceBetween: 0,
 			slidesPerView: 4,
 			freeMode: true,
 			watchSlidesProgress: true
 		});
 		var swiper2 = new Swiper(".mySwiper2", {
-			loop: true,
 			spaceBetween: 0,
 			navigation: {
 				nextEl: ".swiper-button-next",
